fc38a933edc79a9ba84d4bdbe7688af4ad301d8e
aaliyaa47a45 <aaliyaa47a45@gmail.com>
2025-12-25 10:08:37 +0530

    Add files via upload
    Day 1 Python practice

---
+++ day1_python_practice.py
@@ -0,0 +1,14 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+# Day 1 of Python journey
+
+print("Starting my Python journey")
+Starting my Python journey
+name= input("Enter your name: ")
+Enter your name: Aaliya
+print("Hello",name)
+Hello Aaliya
+x=10
+y=5
+print("Sum=",x+y)
+Sum= 15
