4777ffc407a14a6167752bfa5de9223f2090a4b3
aaliyaa47a45 <aaliyaa47a45@gmail.com>
2025-12-26 09:54:41 +0530

    Add files via upload
    Day 2: Variables and input

---
+++ day_2variables.py
@@ -0,0 +1,8 @@
+# Day 2: Variables and Data Types
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+height = float(input("Enter your height in feet:"))
+
+print("Name:", name)
+print("Age:",age)
+print("Height:",height)
