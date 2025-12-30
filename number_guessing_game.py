913f43ffaf22020d3462df52a9c4df16d87adc47
aaliyaa47a45 <aaliyaa47a45@gmail.com>
2025-12-30 14:23:26 +0530

    Built a number guessing game using random module .

---
+++ number_guessing_game.py
@@ -0,0 +1,21 @@
+import random
+
+secret_number = random.randint(1,100)
+attempts=0
+
+print("Welcome to the number guessing game!")
+print("I have selected a number between 1 and 100.")
+
+while True:
+    guess = int(input("Enter your guess:"))
+    attempts=1
+
+    if guess<secret_number:
+        print("Too low!Try again.")
+    elif guess>secret_number:
+        print("Too high!Try again.")
+    else:
+        print("Correct!")
+        print("You guessed the number in",attempts,"attempts.")
+        break
+    
